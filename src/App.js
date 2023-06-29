3f4f585c7405a9ae6d41a567c8f5695345df16c2
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-06-29 12:14:24 -0400

    adding Search bar and new form

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import GalleryCollection from "./components/GalleryCollection";
 import "./App.css";
+import GalleryForm from "./components/GalleryForm";
 
 function App() {
   const [collections, setCollections] = useState([]);
-  useEffect(()=>{
-    fetch(`http://localhost:3041/gallery`).then((r)=>r.json())
-    .then((gallery)=>{setCollections(gallery);});
-  },[]);
+  const [showForm,setShowForm] = useState(false)
+  useEffect(() => {
+    fetch(`http://localhost:3041/gallery`)
+      .then((r) => r.json())
+      .then((gallery) => {
+        setCollections(gallery);
+      });
+  }, []);
+
+  function handleAddCard(newImage){
+    setCollections([...collections,newImage])
+  }
+  function handleButtonClick() {
+    setShowForm((showForm) => !showForm);
+  }
+
+  function searchValue(search) {
+    setCollections(
+      collections.filter((card) =>
+        card.description.toLowerCase().includes(search)
+      )
+    );
+
+   
+  }
   return (
     <div className="App">
-      <Header />
-      <GalleryCollection collections={collections}/>
+      <Header searchValue={searchValue} />
+      {showForm ? <GalleryForm onAdd={handleAddCard}/> : null}
+      <div className="buttonContainer">
+        <button onClick={handleButtonClick}>Add Image</button>
+      </div>
+      <GalleryCollection collections={collections} />
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Search from './Search'
-function Header() {
+function Header({searchValue}) {
   return (
     <header>
         <h1>
             <span className="logo" role="img">
-                <img src='./img.png'/>
+                <img src='./img.png' alt=""/>
             </span>
             Image Gallery
         </h1>
-        <Search />
+        <Search searchValue={searchValue} />
     </header>
   )
 }
